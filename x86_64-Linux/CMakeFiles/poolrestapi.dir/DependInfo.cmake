3ecf7c366c2e1c8964360ee4f25f89c74b4d4e39
nginnever <ginneversource@gmail.com>
2016-12-11 23:10:53 +0100

    12/11

---
+++ x86_64-Linux/CMakeFiles/poolrestapi.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/luckpool/pool/poolrestapi/src/FoundBlocksQuery.cpp" "/home/luckpool/pool/poolrestapi/x86_64-Linux/CMakeFiles/poolrestapi.dir/FoundBlocksQuery.cpp.o"
+  "/home/luckpool/pool/poolrestapi/src/Query.cpp" "/home/luckpool/pool/poolrestapi/x86_64-Linux/CMakeFiles/poolrestapi.dir/Query.cpp.o"
+  "/home/luckpool/pool/poolrestapi/src/poolrestapi.cpp" "/home/luckpool/pool/poolrestapi/x86_64-Linux/CMakeFiles/poolrestapi.dir/poolrestapi.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/luckpool/pool/libp2p/src/include"
+  "/home/luckpool/pool/libp2p/src/include/../../x86_64-Linux/include"
+  "/home/luckpool/pool/poolcore/src/include"
+  "/home/luckpool/pool/poolcore/src/include/../../x86_64-Linux"
+  "/home/luckpool/pool/config4cpp/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ x86_64-Linux/CMakeFiles/poolrestapi.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/poolrestapi.dir/poolrestapi.cpp.o"
+  "CMakeFiles/poolrestapi.dir/Query.cpp.o"
+  "CMakeFiles/poolrestapi.dir/FoundBlocksQuery.cpp.o"
+  "poolrestapi.pdb"
+  "poolrestapi"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/poolrestapi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
